951c1121b3e7a56482f1d3aa987f80aef1cdbfd0
Gear <macgeargear@gmail.com>
2025-04-17 02:51:22 +0700

    feat: login setup

---
+++ src/pages/login.page.ts
@@ -0,0 +1,35 @@
+import { Locator, Page } from "@playwright/test";
+import "dotenv/config";
+
+export class LoginPage {
+  baseUrl = `http://localhost:3000/login`;
+  page: Page;
+  locatorEmail = "#email";
+  locatorPassword = "#password";
+  locatorLoginButton = "#submit-login";
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto(this.baseUrl);
+  }
+
+  async fillEmailPassword(email: string, password: string): Promise<void> {
+    await this.page.locator(this.locatorEmail).fill(email);
+    await this.page.locator(this.locatorPassword).fill(password);
+  }
+
+  async getEmail(): Promise<string> {
+    return await this.page.locator(this.locatorEmail).inputValue();
+  }
+
+  async getPassword(): Promise<string> {
+    return await this.page.locator(this.locatorPassword).inputValue();
+  }
+
+  async clickLogin(): Promise<void> {
+    await this.page.locator(this.locatorLoginButton).click();
+  }
+}
